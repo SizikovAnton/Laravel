cac40308c8f87695eec79ff51b304d2531721083
Sizikov <tosha.sizikov@gmail.com>
2020-06-23 21:48:05 +0300

    Установил debugbar и создал требуемые страницы. Выполнил домашнее задание 1.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Страница приветствия пользователя
+Route::get('/hello.html', function () {
+    $name = request()->has('name') ? request()->get('name') : null;
+    if(is_null($name)) {
+        return "Укажите имя";
+    } else {
+        return "Hello, " . $name;
+    }
+});
+
+//Страница с информацией о проекте
+Route::get('/about.html', function () {
+    return "<h1>Информация о проекте</h1>";
+});
+
+//Страница для вывода одной новости или списка новостей и проверка адреса без ".html"
+Route::get('/news', function () {
+    $id = request()->has('id') ? request()->get('id') : null;
+    if(is_null($id)) {
+        return "<h1>Список новостей</h1>";
+    } else {
+        return "<h1>Новость с ID=$id</h1>";
+    }
+});
